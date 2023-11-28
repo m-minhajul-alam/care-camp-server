d276478c920c586fa25215a9cec0e2e7d666d5a4
Minhajul-alam <alam.minhajul@outlook.com>
2023-11-29 03:27:41 +0600

    Comes upcoming camps data form client side

--- index.js
+++ index.js
@@ -71,6 +71,12 @@ async function run() {
       res.send(result);
     });
 
+    app.post("/upcomingCamps", async (req, res) => {
+      const item = req.body;
+      const result = await upcomingCampCollection.insertOne(item);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
