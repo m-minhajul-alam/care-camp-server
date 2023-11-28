1129a0119f7b21e3b826f75dec5c3063bdfed7d3
Minhajul-alam <alam.minhajul@outlook.com>
2023-11-29 03:12:58 +0600

    Comes users data form clint side

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.eyhxp1w.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -18,7 +16,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -28,18 +26,40 @@ async function run() {
 
     const userCollection = client.db("careCampDB").collection("users");
 
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
 
+      if (existingUser) {
+        return res.send({ message: "User already exists", insertedId: null });
+      }
+
+      try {
+        const result = await userCollection.insertOne(user);
+        res.send({
+          message: "User added successfully",
+          insertedId: result.insertedId,
+        });
+      } catch (error) {
+        console.error("Error inserting user:", error);
+        res
+          .status(500)
+          .send({ message: "Error inserting user", error: error.message });
+      }
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
-
 
 app.get("/", (req, res) => {
   res.send("Care Camp server is running...");
